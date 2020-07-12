cae12fee0b1ae593d7b0bbdbed5c811ac4310a4f
Zhicheng Wang <zhichengwangS@outlook.com>
2020-07-13 00:20:18 +1000

    Add new code

---
+++ 2. Setting Out to C++/myfirst.cpp
@@ -0,0 +1,10 @@
+// myfirst.app -- displays a message
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "Come up";
+    cout << endl;
+    cout << "You don't regret it" << endl;
+    return 0;
+}
